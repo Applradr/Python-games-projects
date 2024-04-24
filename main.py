70539b5283ddf4d8fd5e3def625ee67d3ce3c270
Margaret W.M <123535374+Applradr@users.noreply.github.com>
2024-04-24 18:55:43 +0000

    First commit

--- main.py
+++ main.py
@@ -28,20 +28,20 @@ def race(colors):
           racer.forward(distance)
 
           x,y = racer.pos()
-          if y >= HEIGHT // 2 - 10:
+          if y >= HEIGHT // 2 - 20:
               return colors[turtles.index(racer)]
 
 
 def create_turtle(colors):
   turtles = []
-  spacing = WIDTH // (len(colors)+ 1)
+  spacingx = WIDTH // (len(colors)+ 1)
   for i, color in enumerate(colors):
     racer = turtle.Turtle()
     racer.color(color)
     racer.shape("turtle")
     racer.left(90)
     racer.penup()
-    racer.setpos(-WIDTH // 2 * (i + 1) * spacing, -HEIGHT // 2 + 10)
+    racer.setpos(-WIDTH // 2 + (i + 1) * spacingx, -HEIGHT // 2 + 20)
     racer.pendown()
     turtles.append(racer)
   return turtles
